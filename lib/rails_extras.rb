8a8d8b5d43d159208e9868c939661c7a9f8dc64b
Michał Szyma <michal@szyma.dev>
2014-02-25 21:58:39 +0100

    RSpec::Support::Common module has got #upload_file for method

--- lib/rails_extras.rb
+++ lib/rails_extras.rb
@@ -1,6 +1,7 @@
-require "rails_extras/version"
-require "rails_extras/helpers/tag"
-require "rails_extras/rspec/formatters/numeric_progress"
+require 'rails_extras/version'
+require 'rails_extras/helpers/tag'
+require 'rails_extras/rspec/formatters/numeric_progress'
+require 'rails_extras/rspec/support/common'
 
 class RailsExtras
 
